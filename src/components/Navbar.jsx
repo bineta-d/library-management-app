541514239f34563a686cc4f213aa3bcc341586c7
Bineta Diatta <bineta.diatta21@gmail..com>
2024-09-15 01:49:03 -0400

    favorites list items styled + back to main button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../App';
+import {Link} from 'react-router-dom';
 
 const Navbar = ( ) =>{
     // console.log('Navbar rendering');
     return (
         <div className='navbar'>
             <div>
-                <h1>React Books App</h1>
+                <h1>Your Library</h1>
             </div>
-            <div>Your Favorites</div>
+            <div>
+                <Link to="/favorites"><h3>Your Favorites</h3></Link>
+            </div>
 
         </div>
     )
